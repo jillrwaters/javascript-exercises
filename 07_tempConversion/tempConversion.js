dbe36d3acf2dcada8ef4c98339ee4ab838f0f1db
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-24 14:58:03 -0500

    F tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    temp = (temp-32) * .5556
+    return Math.round(temp *10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
 };
+
+convertToCelsius(32)
+convertToCelsius(100)
+convertToCelsius(-100)
+
+
 
 // Do not edit below this line
 module.exports = {
