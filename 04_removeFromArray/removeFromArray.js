d68ab4d7e5cf56c931dab474551d99c985d9d718
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-23 21:24:56 -0500

    all tests passed. figured out debugger

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...other) {
+    for (let i=0;i<other.length;++i){
+        let arg = other[i]
+        for (let j=0;j<array.length;++j){
+            let ele = array[j]
+            if (arg===ele){
+                array.splice(j,1)
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
