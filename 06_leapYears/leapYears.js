a8c65048626260ce683c3910bcf13228fffe703f
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-24 14:45:16 -0500

    all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    if ((year % 4 === 0) && (year % 100 === 0) && (year % 400 === 0)) return true
+    if ((year % 4 === 0) && !(year % 100 === 0)) return true
 
+    return false
 };
 
+
+leapYears(1996)
+leapYears(1997)
 // Do not edit below this line
 module.exports = leapYears;
+
