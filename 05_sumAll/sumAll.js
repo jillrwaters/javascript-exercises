69a142a83d3dd4f3d3d813fb99054d12ff36db74
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-24 14:08:30 -0500

    all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum = 0;
+
+    if (num1 < 0 || num2 < 0) return "ERROR"
+    if (typeof(num1)!== "number" || typeof(num2) !== "number") return "ERROR"
+    let max = Math.max(num1,num2)
+    let min = Math.min(num1,num2)
+
+    while (min <= max) {
+        sum += min
+        min++
+    }
+
+
+    return sum
 
 };
-
+// sumAll(1, 4)
+// sumAll(1, 4000)
+// sumAll(-10, 4)
+// sumAll(10, "90")
+// sumAll(10, [90, 1])
 // Do not edit below this line
 module.exports = sumAll;
