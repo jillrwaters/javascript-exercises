987303924a280facb83f23c8949ff0a49a8796b6
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-24 17:30:06 -0500

    all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,56 @@
-const add = function() {
+const add = function(n1,n2) {
+    return n1 + n2
+};
+
+const subtract = function(n1,n2) {
+	  return n1 - n2
+};
+
+const sum = function(array) {
+    let result = 0
+    if (array.length === 0) return 0
+    if (array.length === 1) return array[0]
+    
+    array.map(x => result += x)
+    
+    return result
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(array) {
+    let result = 1
+    array.map(x => result *= x)
+
+    return result
 };
 
-const sum = function() {
-	
+const power = function(n,p) {
+    let arr = []
+    let i = 0
+    while (i < p) {
+      arr.push(n)
+      i++
+    } 
+    return multiply(arr)
 };
 
-const multiply = function() {
-
+const factorial = function(n) {
+  let ints = []  // for integers below n, and n
+  
+  if (n === 0 || n === 1) return 1
+    
+    for (let i=1;i<=n;i++){
+      ints.push(i)
+    }
+    return multiply(ints)
+ 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+factorial(0)
+factorial(1)
+factorial(2)
+factorial(5)
+factorial(10)
 
 // Do not edit below this line
 module.exports = {
