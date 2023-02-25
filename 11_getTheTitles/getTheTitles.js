70c24a9c41006a3884ae6ea150f0c21a07b684ec
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-24 22:49:50 -0500

    test passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookList) {
+    let titles = []
 
+    for(let i=0;i<bookList.length;i++){
+        titles.push(bookList[i].title)
+    }
+
+    return titles
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
