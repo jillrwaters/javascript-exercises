48c96f4aef2243afaa8f585467b8176dae045b40
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-24 21:49:59 -0500

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const reverseString = function(string) {
+    let stringArray = string.split('')
+    stringArray.reverse()
+    return stringArray.join('')
 
+}
+
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "")
+
+    if (reverseString(string) === string) return true
+
+    return false
+    
 };
+
+// palindromes('racecar!')
+// palindromes('Racecar!')
+// palindromes('A car, a man, a maraca.')
+// palindromes('Animal loots foliated detail of stool lamina.')
+palindromes('ZZZZ car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
