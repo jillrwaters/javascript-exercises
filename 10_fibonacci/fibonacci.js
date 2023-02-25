9982e014721381019a880ddcf1b18f9ecdb1cb8b
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-24 22:27:31 -0500

    all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return ("OOPS")
+    n = parseInt(n)
+    let sequence = [1, 1, 2]
+    
+    for (let i=2;i<n;i++){
+        let next = sequence[i] + sequence[i-1]
+        sequence.push(next)
+    }
+
+    return sequence[n-1]
 
 };
 
+fibonacci(10)
+fibonacci(15)
+fibonacci(25)
 // Do not edit below this line
 module.exports = fibonacci;
