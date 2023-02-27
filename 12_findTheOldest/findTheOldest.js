64cfff8a3ba794e617e7771781ef232a049ffd0b
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-27 15:26:05 -0500

    all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,3 +1,29 @@
+function assignAges(arr){
+    for (let i=0;i<arr.length;i++){
+      let person = arr[i]
+      if (person.yearOfDeath === undefined){
+        person.age = new Date().getFullYear() - person.yearOfBirth
+      }
+      else {
+        person.age = person.yearOfDeath - person.yearOfBirth
+      }
+    }
+    return people
+}
+
+
+function findTheOldest(arr) {
+    assignAges(arr)
+    let oldest = arr[0]
+    for (let i=1;i<arr.length;i++){
+      let previous = arr[i-1]
+      let current = arr[i]
+      if (current.age > oldest.age || current.age > previous.age) {
+        oldest = current
+      }
+    }
+    return oldest
+}
 
 
 // Do not edit below this line
