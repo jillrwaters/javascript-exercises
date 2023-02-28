9c28dd12ac3cc7802365941f0e3262c5b6c5f149
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-02-27 20:08:37 -0500

    doing this the long and messy way because i do not understand the math in the solution. NOT FINISHED!

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,45 @@
-const caesar = function() {
+const caesar = function(message, shift) {
+    let codes = []
+    let newMessage = ''
+    for (char in message){
+        codes.push(message.charCodeAt(char))
+    }
 
+    let shiftedCodes = []
+    for (i in codes){
+        if (codes[i] < 48){
+            shiftedCodes.push(codes[i])
+        }
+        let x = codes[i] + shift
+        
+        if (x < 65) {
+            shiftedCodes.push(90 - ((65 - x) - 1))
+        }     
+        else if ((x > 90) && (x < 97)) {
+            shiftedCodes.push(65 + ((x - 90) - 1))
+        }     
+        else if (x > 122) {
+            shiftedCodes.push(97 + ((x - 122) - 1))
+        }
+        else {
+            shiftedCodes.push(x)
+        }
+                
+    }
+
+    for (i in shiftedCodes){
+        let x = shiftedCodes[i]
+        newMessage += String.fromCharCode(x)
+    }
+    return newMessage
 };
 
+
+// caesar("hey", 4)
+// caesar('A', 1)
+caesar('Mjqqt!', -5)
+caesar('Hello, World!', 75)
+
+
 // Do not edit below this line
-module.exports = caesar;
+// module.exports = caesar;
